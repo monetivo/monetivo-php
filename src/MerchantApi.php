e890da952abee8c58432108a0f27a3d703e88f8b
Jakub Jasiulewicz <jjasiulewicz@monetivo.com>
2017-03-30 10:25:46 +0200

    Version bump

--- src/MerchantApi.php
+++ src/MerchantApi.php
@@ -30,7 +30,7 @@ class MerchantApi
     /**
      * Merchant API client version
      */
-    const CLIENT_VERSION = '1.0.9';
+    const CLIENT_VERSION = '1.0.10';
 
     /**
      * Name of request headers
